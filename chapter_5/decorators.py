b2bc2b5142c676e5aeafc7c91f68155af33a2887
Faruk Ahmad <faruk.csebrur@gmail.com>
2021-02-23 15:47:47 +0900

    added functools.wraps for preserving decorated functions info in the wrapper function in decorator

--- chapter_5/decorators.py
+++ chapter_5/decorators.py
@@ -2,9 +2,12 @@
 This module defines some decorators for reusability
 """
 
+import functools
+
 def do_twice(func):
     """This decorator executes func twice
     """
+    @functools.wraps(func)
     def wrapper_do_twice(*args, **kwargs):
         """Adding *args and **kwargs will allow
         decorating func whether there is any argument
